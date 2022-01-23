ac976ae095be36b8e062241c6c3a00e29671ff11
Yevhenii Hyzyla <hyzyla@gmail.com>
2022-01-23 14:57:06 +0200

    Tolerate dokku env

--- directory/lib/settings.py
+++ directory/lib/settings.py
@@ -1,8 +1,14 @@
-from pydantic import BaseSettings
+from pydantic import BaseSettings, validator
 
 
 class Settings(BaseSettings):
     DATABASE_URL: str
+
+    @validator('DATABASE_URL', pre=True)
+    def normalize(cls, database_url: str) -> str:
+        if isinstance(database_url, str) and database_url.startswith('postgres://'):
+            return 'postgresql://' + database_url.removeprefix('postgres://')
+        return database_url
 
 
 SETTINGS = Settings()
